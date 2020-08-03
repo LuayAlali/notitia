66550394128f5aa974583772703830a032922a6f
Luay Alali <luayalali@gmail.com>
2020-08-03 16:13:23 -0700

    finished integrating dailyChart, mobileChart,  and added an event listeneer for message field.
    if message is not filled out completely an alert pops up to complete missing field.

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ let trafficData = {
     datasets:[{
         data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
             2500],
-            backgroundColor: 'rgba(116, 119, 191, .3)',
+            backgroundColor: 'rgba(145, 230, 191, .3)',
             borderWidth: 1
     }]
 
@@ -38,11 +38,12 @@ legend: {
 }
 };
 
-let trafficChart = new CharacterData(trafficCanvas, {
+let trafficChart = new Chart(trafficCanvas, {
     type: 'line',
     data: trafficData,
     options: trafficOptions
-});
+    });
+    
 
 const dailyData = {
     labels: ['S','M','T','W','T','F','S'],
@@ -55,7 +56,11 @@ const dailyData = {
 
 };
 
-cosnt dailyOptions = {
+const dailyOptions = {
+    aspectRatio: 2.5,
+    animation: {
+    duration: 0
+    },
     scales: {
         yAxes: [{
             ticks: {
@@ -66,6 +71,60 @@ cosnt dailyOptions = {
     legend: {
         display: false
     }
-}
-
 };
+
+ const dailyCanvas = document.getElementById('daily-chart');
+let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
+});
+
+const mobileCanvas = document.getElementById('mobile-users');
+
+const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+    label: '# of Users',
+    data: [2000, 550, 500],
+    borderWidth: 0,
+    backgroundColor: [
+    '#7477BF',
+    '#78CF82',
+    '#51B6C8'
+    ]
+    }]
+};
+
+const mobileOptions = {
+    legend: {
+    position: 'right',
+    labels: {
+    boxWidth: 20,
+    fontStyle: 'bold'
+    }
+    }
+    };
+
+    let mobileChart = new Chart(mobileCanvas, {
+        type: 'doughnut',
+        data: mobileData,
+        options: mobileOptions
+        });
+
+        const user = document.getElementById('userField');
+        const message = document.getElementById('messageField')
+        const send = document.getElementById('send');
+
+        send.addEventListener('click', () => {
+            if(user.value === '' && message.value ===''){
+                alert("Please fill out user and message fields before sending");
+            } else if (user.value === ""){
+                alert("Please fill out user field before sending");
+            } else if (message.value === ''){
+                alert("Please fill out message field before sending");
+            } else {
+              alert(`Message successfully sent to: ${user.value}`);
+            }
+
+        });
